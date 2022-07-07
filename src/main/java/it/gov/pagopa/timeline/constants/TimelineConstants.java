bfed38ba51d77a501050fd6f46ba018c5d894de4
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-07-07 14:41:21 +0200

    [ISB-172] Added TimelineConstants

---
+++ src/main/java/it/gov/pagopa/timeline/constants/TimelineConstants.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.timeline.constants;
+
+public class TimelineConstants {
+
+  public static final String ERROR_MANDATORY_FIELD = "The field is mandatory!";
+
+  private TimelineConstants() {
+  }
+}
