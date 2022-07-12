723bdc23cd4f101f3b92a717b6e6ea0eb8badbe4
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-07-12 09:19:01 +0200

    [ISB-165] Clean code

--- src/test/java/it/gov/pagopa/timeline/service/TimelineServiceTest.java
+++ src/test/java/it/gov/pagopa/timeline/service/TimelineServiceTest.java
@@ -35,9 +35,6 @@ class TimelineServiceTest {
   @MockBean
   TimelineRepository timelineRepositoryMock;
 
-  @MockBean
-  TimelineProducer timelineProducer;
-
   @Autowired
   TimelineService timelineService;
 
