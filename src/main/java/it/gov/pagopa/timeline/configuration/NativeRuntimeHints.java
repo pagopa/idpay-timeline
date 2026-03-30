4d4d544090c84995daf7593c82e8ce993f69ae90
and-mora <andrea.morabito@pagopa.it>
2026-03-30 13:39:27 +0200

    feat: add Kafka SASL compatibility hints for native reflection

--- src/main/java/it/gov/pagopa/timeline/configuration/NativeRuntimeHints.java
+++ src/main/java/it/gov/pagopa/timeline/configuration/NativeRuntimeHints.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.timeline.configuration;
 
 import it.gov.pagopa.common.logback.IgnoreCasePropertyEqualityCondition;
+import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -11,6 +12,11 @@ import org.springframework.jmx.export.metadata.ManagedNotification;
 import org.springframework.jmx.export.metadata.ManagedOperation;
 import org.springframework.jmx.export.metadata.ManagedOperationParameter;
 import org.springframework.jmx.export.metadata.ManagedResource;
+
+import javax.security.auth.Subject;
+import java.lang.reflect.Method;
+import java.security.AccessControlContext;
+import java.security.AccessController;
 
 public class NativeRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -29,6 +35,7 @@ public class NativeRuntimeHints implements RuntimeHintsRegistrar {
     registerJmxMetadataType(hints, ManagedOperation.class);
     registerJmxMetadataType(hints, ManagedNotification.class);
     registerJmxMetadataType(hints, ManagedOperationParameter.class);
+    registerKafkaSaslCompatibilityHints(hints);
     hints.resources().registerPattern("org/joda/time/tz/data/**");
   }
 
@@ -37,5 +44,32 @@ public class NativeRuntimeHints implements RuntimeHintsRegistrar {
         type,
         MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
         MemberCategory.INVOKE_PUBLIC_METHODS);
+  }
+
+  private static void registerKafkaSaslCompatibilityHints(RuntimeHints hints) {
+    hints.reflection().registerType(AccessController.class);
+    hints.reflection().registerType(AccessControlContext.class);
+    hints.reflection().registerType(Subject.class);
+
+    hints.reflection().registerMethod(
+        method(AccessController.class, "doPrivileged", java.security.PrivilegedAction.class),
+        ExecutableMode.INVOKE);
+    hints.reflection().registerMethod(method(AccessController.class, "getContext"), ExecutableMode.INVOKE);
+    hints.reflection().registerMethod(method(Subject.class, "getSubject", AccessControlContext.class), ExecutableMode.INVOKE);
+    hints.reflection().registerMethod(
+        method(Subject.class, "doAs", Subject.class, java.security.PrivilegedExceptionAction.class),
+        ExecutableMode.INVOKE);
+    hints.reflection().registerMethod(method(Subject.class, "current"), ExecutableMode.INVOKE);
+    hints.reflection().registerMethod(
+        method(Subject.class, "callAs", Subject.class, java.util.concurrent.Callable.class),
+        ExecutableMode.INVOKE);
+  }
+
+  private static Method method(Class<?> type, String name, Class<?>... parameterTypes) {
+    try {
+      return type.getDeclaredMethod(name, parameterTypes);
+    } catch (NoSuchMethodException exception) {
+      throw new IllegalStateException("Missing JDK method required for Kafka native SASL compatibility", exception);
+    }
   }
 }
--- src/test/java/it/gov/pagopa/timeline/configuration/NativeRuntimeHintsTest.java
+++ src/test/java/it/gov/pagopa/timeline/configuration/NativeRuntimeHintsTest.java
@@ -8,6 +8,10 @@ import org.springframework.jmx.export.metadata.AbstractJmxAttribute;
 import org.springframework.jmx.export.metadata.ManagedOperationParameter;
 import org.springframework.jmx.export.metadata.ManagedResource;
 
+import javax.security.auth.Subject;
+import java.security.AccessControlContext;
+import java.security.AccessController;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NativeRuntimeHintsTest {
@@ -25,6 +29,12 @@ class NativeRuntimeHintsTest {
     assertThat(RuntimeHintsPredicates.reflection().onType(ManagedResource.class))
         .accepts(hints);
     assertThat(RuntimeHintsPredicates.reflection().onType(ManagedOperationParameter.class))
+        .accepts(hints);
+    assertThat(RuntimeHintsPredicates.reflection().onType(AccessController.class))
+        .accepts(hints);
+    assertThat(RuntimeHintsPredicates.reflection().onType(AccessControlContext.class))
+        .accepts(hints);
+    assertThat(RuntimeHintsPredicates.reflection().onType(Subject.class))
         .accepts(hints);
     assertThat(RuntimeHintsPredicates.resource().forResource("org/joda/time/tz/data/ZoneInfoMap"))
         .accepts(hints);
