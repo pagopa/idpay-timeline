1c934c50c445d8c228c2f2979c96038f77061199
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2026-04-08 13:03:32 +0200

    fix: kafka instrumentation with otel (#211)

---
+++ src/main/java/it/gov/pagopa/timeline/configuration/KafkaTelemetryConfiguration.java
@@ -0,0 +1,30 @@
+package it.gov.pagopa.timeline.configuration;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.kafkaclients.v2_6.KafkaTelemetry;
+import org.springframework.cloud.stream.binder.kafka.config.ClientFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration(proxyBeanMethods = false)
+public class KafkaTelemetryConfiguration {
+
+  @Bean
+  KafkaTelemetry kafkaTelemetry(OpenTelemetry openTelemetry) {
+    return KafkaTelemetry.create(openTelemetry);
+  }
+
+  @Bean
+  ClientFactoryCustomizer otelKafkaClientFactoryCustomizer(KafkaTelemetry kafkaTelemetry) {
+    return new ClientFactoryCustomizer() {
+      @Override
+      public void configure(ProducerFactory<?, ?> producerFactory) {
+        if (producerFactory instanceof DefaultKafkaProducerFactory<?, ?> defaultProducerFactory) {
+          defaultProducerFactory.addPostProcessor(kafkaTelemetry::wrap);
+        }
+      }
+    };
+  }
+}
